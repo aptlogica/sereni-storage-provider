a41782336eb880352f6f503a32b5ac439baeb621
Ajit Khatke <ajit.khatke@aptlogica.com>
2026-01-02 13:27:24 +0530

    updated default port to avoid port conflicts

--- docs/docs.go
+++ docs/docs.go
@@ -238,7 +238,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8083",
+	Host:             "localhost:5050",
 	BasePath:         "/api/v1",
 	Schemes:          []string{},
 	Title:            "Sereni Storage Provider API",
