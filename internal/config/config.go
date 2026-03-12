9565737f1ed5a75612117fe5063c8e9998125168
gaurav-gaikwad-12071998 <122888092+gaurav-gaikwad-12071998@users.noreply.github.com>
2026-03-12 10:18:38 +0530

    Added copyright headers

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2026 Aptlogica Technologies Private Limited
+// SPDX-License-Identifier: MIT
+// Websites: https://www.aptlogica.com | https://www.serenibase.com
+// Support: support@aptlogica.com | support@serenibase.com
 package config
 
 import (
